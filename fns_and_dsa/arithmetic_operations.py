a6340b80d2373b1b8d32c96b53063eeb0ed332d7
Eioel D. <eioeldagnachew@gmail.com>
2025-08-13 17:40:10 +0300

    Correct spelling for subtract in match case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@ def perform_operation(num1, num2, operation):
     match operation:
         case "add":
             return num1 + num2
-        case "subract":
+        case "subtract":
             return num1 - num2
         case "multiply":
             return num1 * num2

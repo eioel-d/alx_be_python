bb9874cb9bde9f437bf8c1ca34f8a170b2db809a
Eioel D. <eioeldagnachew@gmail.com>
2025-08-13 17:37:40 +0300

    Add functions and data structures directory

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                print("Invalid operation, cannot divide by 0")
+            else:
+                return num1 / num2
+        case _:
+            print("Invalid operation")

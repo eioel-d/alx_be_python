b92e2e309af90fa8577050236779fd474d522b35
Eioel D. <eioeldagnachew@gmail.com>
2025-08-13 18:07:25 +0300

    Fix the prompt to add items

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
         
         if choice == "1":
-            item = input("Please add an item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == "2":
             item = input("Write the name of the item to remove: ")

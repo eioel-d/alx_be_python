1a3080e0fe3a76a3ddf56e07915481bb49c7c9dd
Eioel D. <eioeldagnachew@gmail.com>
2025-08-13 10:34:13 +0300

    Add reminder and note to the output string according to the time

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,23 +5,23 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"{task} is a high priority task that requires immediate action today!")
+            print(f"Reminder: {task} is a high priority task that requires immediate action today!")
         elif time_bound == "no":
-            print(f"{task} is a high priority task, get to it when you can.")
+            print(f"Note: {task} is a high priority task, get to it when you can.")
         else:
             print(f"You have not entered a proper time for: {task}.")
     case "medium":
         if time_bound == "yes":
-            print(f"{task} is a medium priority task that requires an action today!")
+            print(f"Reminder: {task} is a medium priority task that requires an action today!")
         elif time_bound == "no":
-            print(f"{task} is a medium priority task, get to it when you can.")
+            print(f"Note: {task} is a medium priority task, get to it when you can.")
         else: 
             print(f"You have not entered a proper time for: {task}.")
     case "low":
         if time_bound == "yes":
-            print(f"{task} is low priority task that requires an action!")
+            print(f"Reminder: {task} is low priority task that requires an action!")
         elif time_bound == "no":
-            print(f"{task} is low priority task, get to it when you can.")
+            print(f"Note: {task} is low priority task, get to it when you can.")
         else:
             print(f"You have not entered a proper time for: {task}.")
     case _:
